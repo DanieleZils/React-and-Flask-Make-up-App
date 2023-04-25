8f124f66805434750c5ecd5dc93bc4716a2d79a1
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2023-04-25 10:08:59 -0400

    cleanup complete, installed react

--- server/app.py
+++ server/app.py
@@ -10,16 +10,6 @@ from sqlalchemy.exc import IntegrityError
 from config import app
 from models import db
 
-# Views go here!
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-
-migrate = Migrate(app, db)
-
-db.init_app(app)
-api = Api(app)
 
 
 if __name__ == '__main__':
--- server/config.py
+++ server/config.py
@@ -10,7 +10,7 @@ from flask_bcrypt import Bcrypt
 
 # Instantiate app, set attributes
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///localhost'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
