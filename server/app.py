2eed3339f20c9242886cacbbaf4d80d673f0a55f
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2023-04-26 12:04:56 -0400

    added try/except and error handling msg to app.py

--- server/app.py
+++ server/app.py
@@ -21,24 +21,26 @@ api.add_resource(Home, '/')
 class Signup(Resource):
 
     def post(self):
+        data = request.get_json()
 
-        username = request.get_json()['username']
-        password = request.get_json()['password']
-
-        if username and password:
-
-            new_user = User(username=username)
-            new_user.password_hash = password
+        try:
+            new_user = User(username=data['username'])
+            new_user.password_hash = data['password']
             db.session.add(new_user)
             db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+            return make_response({"error": "Username already exists"}, 422)
+        except ValueError as ve:
+            return make_response({"error": ve.__str__()}, 422)
+        except Exception as e:
+            return make_response({"errors": [e.__str__()]}, 422)
 
-            session['user_id'] = new_user.id
+        session['user_id'] = new_user.id
+        return make_response(new_user.to_dict(), 201)
 
-            return make_response(new_user.to_dict(), 201)
-        
-        return {'error': '422 Unprocessable Entity'}, 422
-    
 api.add_resource(Signup, '/signup')
+
 
 
 class CheckSession(Resource):
@@ -56,22 +58,30 @@ class CheckSession(Resource):
 api.add_resource(CheckSession, '/checksession')
 
 class Login(Resource):
-
     def post(self):
+        data = request.get_json()
 
-        username = request.get_json()['username']
-        password = request.get_json()['password']
+        username = data['username']
+        password = data['password']
 
-        user = User.query.filter(User.username == username).first()
+        try:
+            user = User.query.filter(User.username == username).first()
 
-        if user.authenticate(password):
+            if user is None:
+                raise ValueError("Username doesn't exist, you must sign-up")
 
-            session['user_id'] = user.id
-            return user.to_dict(), 200
+            if user.authenticate(password):
+                session['user_id'] = user.id
+                return user.to_dict(), 201
 
-        return {'error': '401 Unauthorized'}, 401
+            return {'error': 'Invalid Password'}, 401
+        except ValueError as ve:
+            return make_response({"error": ve.__str__()}, 404)
+        except Exception as e:
+            return make_response({"errors": [e.__str__()]}, 422)
 
 api.add_resource(Login, '/login')
+
 
 
 class Logout(Resource):
