368032930edbbe14fe2f2103fde66090367ca4ec
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2023-05-07 19:26:11 -0400

    stashed changes

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,13 +1,12 @@
 import React from "react";
 import FeaturedProducts from "./FeaturedProducts";
-import { EmblaCarousel } from "./EmblaCarousel";
+
 
 
 function Home(){
 
     return (
     <div>
-        <EmblaCarousel />
           <div className="homeFeat">
            <h2 style={{fontFamily:"Roboto"}}>Featured Products</h2>
             <FeaturedProducts />
